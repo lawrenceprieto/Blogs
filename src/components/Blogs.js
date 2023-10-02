81aeb27714c4db5564e21433aff800fc3feaf70d
Lawrence Prieto <keithprietolawrence@gmail.com>
2023-10-02 22:17:50 +0800

    add another card

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,24 +1,41 @@
-import thumbnail from "../assets/poster/reacttogithub-poster.gif";
+import react from "../assets/poster/reacttogithub-poster.gif";
+import vite from "../assets/poster/VitetoGithubposter.gif";
 import Card from "./Card";
 
 function Blogs() {
 
     return (
         <>
-            <Card
-                title="Deploy React App to Github Pages"
-                date="Sept 26"
-                description="The first step in displaying your abilities and ideas to 
-                the world in the ever changing field of web development 
-                is to create a beautiful React application. You require 
-                a trustworthy hosting service if you want to share your 
-                creation with a genuinely worldwide audience. Enter GitHub 
-                Pages, a simple and affordable method for distributing your 
-                React project and making it available to anybody with an 
-                internet connection."
-                link="deploy-reactapp-to-ghpages"
-                thumbnail={thumbnail}    
-            />
+            <div className="d-flex flex-column">
+                <Card
+                    title="Deploy Vite React App to Github Pages"
+                    date="Oct 2"
+                    description="The first step in displaying your abilities and ideas to 
+                    the world in the ever changing field of web development 
+                    is to create a beautiful React application. You require 
+                    a trustworthy hosting service if you want to share your 
+                    creation with a genuinely worldwide audience. Enter GitHub 
+                    Pages, a simple and affordable method for distributing your 
+                    React project and making it available to anybody with an 
+                    internet connection."
+                    link="deploy-vitereactapp-to-ghpages"
+                    thumbnail={vite}    
+                />
+                <Card
+                    title="Deploy React App to Github Pages"
+                    date="Sept 26"
+                    description="The first step in displaying your abilities and ideas to 
+                    the world in the ever changing field of web development 
+                    is to create a beautiful React application. You require 
+                    a trustworthy hosting service if you want to share your 
+                    creation with a genuinely worldwide audience. Enter GitHub 
+                    Pages, a simple and affordable method for distributing your 
+                    React project and making it available to anybody with an 
+                    internet connection."
+                    link="deploy-reactapp-to-ghpages"
+                    thumbnail={react}    
+                />
+            </div>
         </>
     );
 }
