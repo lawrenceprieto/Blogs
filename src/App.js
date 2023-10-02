538e45f0502063e5695b87b76c1b309d08550d05
Lawrence Prieto <keithprietolawrence@gmail.com>
2023-10-02 21:46:03 +0800

    add another page for blog

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Blogs from "./components/Blogs";
 import DeployReactToGHPages from "./pages/DeployReactToGHPages";
 import NoPage from "./pages/NoPage";
+import DeployViteReactToGHPages from "./pages/DeployViteReactToGHPages";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/blogs" element={<Blogs />} />
           <Route path="/blogs/deploy-reactapp-to-ghpages" element={<DeployReactToGHPages />} />
+          <Route path="/blogs/deploy-vitereactapp-to-ghpages" element={<DeployViteReactToGHPages />} />
           <Route path="*" element={<NoPage />} />
       </Routes>
     </BrowserRouter>
